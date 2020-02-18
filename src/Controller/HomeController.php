47b870bc94476b0e8142baeabfc7cd737938e5f3
Muratshin Elya <ptrwowhochy@gmail.com>
2020-02-18 21:44:57 +0500

    add homecontroller route

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,13 @@
+<?
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+class HomeController
+{
+    public function number()
+    {
+        $number = random_int(0, 100);
+        return new Response(
+            '<html><body>Lucky number' .$number .'</body></html>'
+        );
+    }
+}
